7193d7bf4fa346be5fd1575ee2088164edaf4247
sbullocks <sbullocks@gmail.com>
2022-11-06 16:27:29 -0500

    Defined column for tag id on product tag model.

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -25,6 +25,14 @@ ProductTag.init(
         unique: false
       }
     },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+        unique: false
+      }
+    },
   },
   {
     sequelize,
