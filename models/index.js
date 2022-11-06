678749c281e99a0d6b7d713f32f957d4b0752db8
sbullocks <sbullocks@gmail.com>
2022-11-06 15:58:00 -0500

    added categories have many products.

--- models/index.js
+++ models/index.js
@@ -9,7 +9,10 @@ Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
 
-// Categories have many Products
+// Categories have many Products.
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
 
