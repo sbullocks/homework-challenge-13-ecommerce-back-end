299ea06d375a6cdce3a9fc66966862f805e41975
sbullocks <sbullocks@gmail.com>
2022-11-06 15:45:37 -0500

    added async/await and try/catch for the put method on tag route.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -54,8 +54,20 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // Updates a tag's name by its `id` value.
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: { id: req.params.id }
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No category was found with the specified id.' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
