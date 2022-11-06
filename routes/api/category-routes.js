ec2f0dad765eea308a39e7ac1d5bfc7fa01cb6c9
sbullocks <sbullocks@gmail.com>
2022-11-06 14:25:20 -0500

    added async/await and catch error for id page.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,9 +14,23 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const catetoryInfo = await Category.findByPk(req.params.id, {
+      include: [{model: Product,
+      attributes: ['product_name', 'price', 'stock'],
+    }]
+    });
+    if (!catetoryInfo) {
+      res.status(404).json({message: 'No category found with specified id.'});
+      return;
+    }
+    res.status(200).json(catetoryInfo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
