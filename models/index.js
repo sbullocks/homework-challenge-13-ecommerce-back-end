96d8f7acf08f1e21504e2c1a81f6cc06281cff32
sbullocks <sbullocks@gmail.com>
2022-11-06 15:57:07 -0500

    added products belongs to category.

--- models/index.js
+++ models/index.js
@@ -1,10 +1,13 @@
-// import models
+// Import models.
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category.
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
 
