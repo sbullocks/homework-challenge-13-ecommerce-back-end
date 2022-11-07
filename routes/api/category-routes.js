c3aed2c139c70b8d4f833ae5b3efbf9d6551d31d
sbullocks <sbullocks@gmail.com>
2022-11-07 16:54:36 -0500

    added async/await and try/catch for the put method on category route.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -51,8 +51,22 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // Updates a category by its `id` value.
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: { id: req.params.id },
+    });
+    if (!tagData) {
+      res
+        .status(404)
+        .json({ message: "No category was found with the specified id." });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // Delete method route.
